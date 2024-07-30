dea343ecf66f7ee72cf11931061d3026a2a56a2b
Oghenetega Idogun <otegaid@gmail.com>
2024-07-30 11:21:39 -0700

    Added Two Sections to the Index Page

--- js/navmenu.js
+++ js/navmenu.js
@@ -37,10 +37,11 @@ function updateBurgerColorForOpenMenu() {
 }
 
 const sectionColors = {
-    0: 'white',
+    0: '#ffffff',
     1: '#2a5298',
-    2: 'white',
+    2: '#ffffff',
     3: '#2a5298',
+    4: '#ffffff',
 };
 
 // Function to update the burger color based on the current active section
