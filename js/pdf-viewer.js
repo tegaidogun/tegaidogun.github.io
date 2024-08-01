5e5f97ca555f7df5a49871d5b2ef82719cc4038b
Oghenetega Idogun <otegaid@gmail.com>
2024-07-31 17:53:28 -0700

    Quick Fix for Resume Scaling on Lower Resolution Devices

--- js/pdf-viewer.js
+++ js/pdf-viewer.js
@@ -2,7 +2,6 @@ const url = 'assets/resume.pdf';
 
 let pdfDoc = null,
     pageNum = 1,
-    scale = 2, // Set initial scale (zoom level)
     canvas = document.querySelector('#pdf-render'),
     ctx = canvas.getContext('2d');
 
@@ -14,21 +13,25 @@ pdfjsLib.getDocument(url).promise.then(pdfDoc_ => {
 
 function renderPage(num) {
     pdfDoc.getPage(num).then(page => {
-        const viewport = page.getViewport({ scale });
-        const outputScale = window.devicePixelRatio || 1;
+        // Calculate scale based on the canvas width
+        const viewport = page.getViewport({ scale: 1 });
+        const canvasWidth = window.innerWidth;
+        const scale = canvasWidth / viewport.width;
+        const scaledViewport = page.getViewport({ scale });
 
         // Adjust canvas size to match scale and device pixel ratio
-        canvas.width = Math.floor(viewport.width * outputScale);
-        canvas.height = Math.floor(viewport.height * outputScale);
-        canvas.style.width = Math.floor(viewport.width) + "px";
-        canvas.style.height = Math.floor(viewport.height) + "px";
+        const outputScale = window.devicePixelRatio || 1;
+        canvas.width = Math.floor(scaledViewport.width * outputScale);
+        canvas.height = Math.floor(scaledViewport.height * outputScale);
+        canvas.style.width = Math.floor(scaledViewport.width) + "px";
+        canvas.style.height = Math.floor(scaledViewport.height) + "px";
 
         // Scale the context to ensure sharpness
         ctx.setTransform(outputScale, 0, 0, outputScale, 0, 0);
 
         const renderContext = {
             canvasContext: ctx,
-            viewport: viewport
+            viewport: scaledViewport
         };
 
         page.render(renderContext).promise.then(() => {
